c81b2033286a96ca06ffdecfa8741c928a5b4594
vgita <22777783+vgita@users.noreply.github.com>
2024-11-21 22:55:14 +0200

    streaming text generation

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,4 +1,5 @@
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
@@ -8,10 +9,12 @@ from app.chat.memories.sql_memory import build_memory
 def build_chat(chat_args: ChatArgs):
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
     
-    return ConversationalRetrievalChain.from_llm(
+    return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
+        condense_question_llm=condense_question_llm,
         memory=memory,
         retriever=retriever,
     )
